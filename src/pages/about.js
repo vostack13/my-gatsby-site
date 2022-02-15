f45bd1c110f226e65170f397c634526e1301641b
Stanislav Vozikov <stanislav.vozikov@gmail.com>
2022-02-15 16:33:14 +0300

    Add runtime logic

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,24 @@ import * as React from 'react'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
+  const [isComplete, setIsComplete] = React.useState(false);
+  const onSubmit = React.useCallback((event) => {
+    event.preventDefault();
+
+    setIsComplete(true);
+  }, [])
+
+
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+
+      {isComplete
+        ? <div>{'Complete!!!'}</div>
+        : <form onSubmit={onSubmit}>
+            <input name="name" />
+          </form>
+      }
     </Layout>
   )
 }
